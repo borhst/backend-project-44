a1d1047c5f8d38cd7feae264f060073cc4d6fc44
starborh <boris.starshinin@gmail.com>
2022-10-08 18:17:17 +0300

    add brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+import brainPrime from '../games/brain-prime.js';
+brainPrime();
---
+++ games/brain-prime.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+import taskPart from '../src/index.js';
+
+const brainPrime = () => {
+    const task = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+    const questions = [];
+    const answers = [];
+
+    for (let i = 0; i < 3; i += 1) {
+
+        let question = Math.floor(Math.random() * 100) + 2;
+        questions.push(question);
+
+        for (let j = 2; j <= question; j += 1) {
+            if (question === j) {
+                answers.push('yes');
+                break;
+            }
+            else if (question % j === 0) {
+                answers.push('no');
+                break;
+            }
+            
+        }
+    }
+
+    taskPart(task, questions, answers);
+}
+
+export default brainPrime;
